a84ea795c86460be0b11c2fe5e49f92c98d35cdf
themanyouknow <artyomik.arturik@gmail.com>
2024-08-13 21:47:51 -0700

    last changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
 import "./contact.scss";
+import contact from '../../images/vector-contact-us-background.jpg';
 
 const Contact = () => {
   return (
@@ -13,6 +14,7 @@ const Contact = () => {
         Phone Number: <a href="tel:+9167996387">(916)-799-6387</a> (only use
         number for when email doesn't respond)
       </span>
+      <img className="contactImage" src={contact} alt="contact" />
     </div>
   );
 };
